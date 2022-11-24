5ff9e77b25ee7600af755b23ae34ccbcf60c7bd8
Your Name <you@example.com>
2022-11-24 15:23:36 +0400

    div commit test

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+<div></div>
     <div class="wrapper">
       <HelloWorld msg="Shopping Market" />
 
