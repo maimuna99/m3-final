1f7a17bb8af5df9f7e1ea4bf9862584d7bdecede
Your Name <you@example.com>
2022-11-28 16:25:05 +0400

    last update

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,6 +2,7 @@
 	import { ref } from 'vue';
 	const response = ref({});
 
+
   let imges = document.images
 let primary = document.getElementById("primary")
 
--- src/components/StarRating.vue
+++ src/components/StarRating.vue
@@ -1,11 +1,9 @@
 <script setup>
 import { ref } from 'vue';
 
-const rate = toRef(props, 'rate');
 </script>
 
 <template>
-<div id="rating-info">{{rate}}</div>
 <div>
         <b>Rating: </b>{{response.rating}}/5
         <span v-if="response.rating >= 4.5">⭐ ⭐ ⭐ ⭐ ⭐</span>
